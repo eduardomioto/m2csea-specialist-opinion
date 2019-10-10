305cd7bb0224ac68ecb1591fc04d87ad2ca9de47
Eduardo Mioto <eduardo.mioto@philips.com>
2019-10-10 20:33:08 -0300

    update
    update

--- src/main/java/br/com/mioto/cloud/SpecialistOpinionApp.java
+++ src/main/java/br/com/mioto/cloud/SpecialistOpinionApp.java
@@ -4,22 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import lombok.extern.slf4j.Slf4j;
 
 @EnableTransactionManagement
 @EnableNeo4jRepositories("br.com.mioto.cloud.dao.repo")
 @Configuration
 @EnableAutoConfiguration
-@EnableDiscoveryClient
 @EnableConfigurationProperties
-@EnableFeignClients
-@Slf4j
 @SpringBootApplication
 public class SpecialistOpinionApp {
 
